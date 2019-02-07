b44561e57a8f5c7a389e961c2b32a4e0fafdea90
Maxime Robert <maxime.robert1992@gmail.com>
2019-02-07 17:47:09 +0000

    chore: clean up unneeded component into the lib

--- projects/ngx-favicon/src/lib/ngx-favicon.module.ts
+++ projects/ngx-favicon/src/lib/ngx-favicon.module.ts
@@ -1,9 +1,4 @@
 import { NgModule } from '@angular/core';
-import { NgxFaviconComponent } from './ngx-favicon.component';
 
-@NgModule({
-  declarations: [NgxFaviconComponent],
-  imports: [],
-  exports: [NgxFaviconComponent],
-})
+@NgModule()
 export class NgxFaviconModule {}
--- projects/ngx-favicon/src/public_api.ts
+++ projects/ngx-favicon/src/public_api.ts
@@ -3,5 +3,4 @@
  */
 
 export * from './lib/ngx-favicon.service';
-export * from './lib/ngx-favicon.component';
 export * from './lib/ngx-favicon.module';
